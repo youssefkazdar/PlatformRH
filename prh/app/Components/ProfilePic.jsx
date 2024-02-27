90b2e0b7a893a19cef7277eca76b2964b8f33d13
Skander <hakounaiskander@gmail.com>
2024-02-27 22:32:01 +0100

    modify and edit files

---
+++ prh/app/Components/ProfilePic.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './global.css';
+
+function ProfilePic({ imageUrl }) {
+  return (
+    <div className="flex justify-center">
+      <div className="mt-4">
+        <img src={imageUrl || 'https://via.placeholder.com/250'} alt="Profile" className="w-40 h-40 rounded-full object-cover" />
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePic;
