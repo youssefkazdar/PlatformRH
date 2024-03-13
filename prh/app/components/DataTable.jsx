62f38a267eca877a3ee9bd10761b834d797640b1
youssefkazdar <youssefkazdar51@gmail.com>
2024-03-13 17:26:58 +0100

    done

--- prh/app/components/DataTable.jsx
+++ prh/app/components/DataTable.jsx
@@ -1,16 +1,19 @@
-
 import React from 'react';
+
 
 const DataTable = () => {
   return (
     <div className="flex  flex-col p-4 gap-2">  
 
       <button className="self-start rounded-2xl py-2 px-6 bg-[#2F5B96] text-white">Ajouter Membre</button>
+      <div class="flex-auto mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800 w-auto">
+        
       <div className='flex justify-between items-center'>
 <h2 className='font-bold font-sans'> Tous les Membre</h2>
 
-      <input className=" bg-gray-100 px-6 py-3" type="text" placeholder="Search.." />
+      <input className=" bg-gray-100 px-6 py-3 rounded-2xl" type="text" placeholder="Search.." />
       </div>
+      
 
       <table className=" ml-2 border-collapse border border-gray-300 w-full">
         <thead>
@@ -34,8 +37,36 @@ const DataTable = () => {
             <td className="border border-gray-300 p-2">Nom</td>
             <td className="border border-gray-300 p-2">Nom</td>
           </tr>
+          <tr className="border border-gray-300">
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+          </tr>
+          <tr className="border border-gray-300">
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+          </tr>
+          <tr className="border border-gray-300">
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+            <td className="border border-gray-300 p-2">Nom</td>
+          </tr>
         </tbody>
       </table>
+    </div>
     </div>
   );
 };
