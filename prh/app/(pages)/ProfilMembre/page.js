b9e8c8ac4e431bd2b956af649795174ae0782f23
Meriem <meriemmansour02@yahoo.fr>
2024-03-16 17:06:49 +0100

    modify and edit files

--- prh/app/(pages)/ProfilMembre/page.js
+++ prh/app/(pages)/ProfilMembre/page.js
@@ -1,10 +1,34 @@
-import Card from "./components/card";
-import Layout from "./layout";
 
-function ProfilMembre(){
-   return (<div>
-       <Layout></Layout>    
-   </div>)
+import CardMembre from '@/app/components/CardMembre'; 
+import ButtonMembre from '@/app/components/ButtonMembre'; 
+
+function ProfilMembre({ children }) {
+  const handleButtonClick = () => {
+    console.log('Button clicked!');
+  };
+
+  return (
+    <div className="layout">
+      <CardMembre
+        picture="https://via.placeholder.com/150"
+        name="SADOT BENOIT"
+        pole="Membre pole projet et prospection"
+        phoneNumber="26 228 274"
+        specialty="1ere génie informatique"
+      />
+      <div className="flex justify-center mt-2 mr-auto space-x-6 ">
+        <ButtonMembre onClick={handleButtonClick}>Modifier</ButtonMembre> 
+        <ButtonMembre onClick={handleButtonClick}>Supprimer</ButtonMembre> 
+      </div>
+    </div>
+  );
 }
 
 export default ProfilMembre;
+
+
+
+
+
+
+
