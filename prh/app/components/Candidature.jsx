602034f4cd7ff4bd6f3b473b2b9d2644f1e6f24e
youssefkazdar <youssefkazdar51@gmail.com>
2024-03-18 15:38:43 +0100

    Candidat

---
+++ prh/app/components/Candidature.jsx
@@ -0,0 +1,160 @@
+import React from 'react'
+import Image from "next/image";
+
+
+const Candidature = () => {
+  return (
+    <div>
+    <div className="flex  flex-col p-4 gap-2">
+        <div className='justify-between '>
+       <button className="self-start rounded-2xl py-2 px-6 bg-[#2F5B96] text-white">Espace Recruteur</button>
+       <input className="bg-[#2F5B96] px-6 py-2 rounded-2xl absolute top-0 right-0 h-12 w-50 mt-3" type="text" placeholder="⌕ Rechercher" />
+       </div>
+       <br />
+       <div className=' flex justify-between '>
+       <h1 className='font-bold font-sans'>Recrutement pour 2023 </h1>
+       <button className='border px-5'   >   Filtrer</button>
+       
+       </div>
+       
+
+    </div>
+
+
+
+
+
+
+    <div className='grid lg:grid-cols-3 gap-2'>
+        <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+          <h2 className='font-bold font-sans text-center'>Candiat</h2>
+        <div className='flex justify-evenly '>
+   <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
+   
+  
+   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+       </div>
+       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+      
+       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+     
+       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans' >0</h3>
+       <br />
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+
+
+
+  </div>    
+  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+          <h2 className='font-bold font-sans text-center'>Candiat</h2>
+        <div className='flex justify-evenly '>
+   <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
+   
+  
+   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+       </div>
+       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+      
+       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+     
+       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans' >0</h3>
+       <br />
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+
+
+
+  </div>    
+  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+          <h2 className='font-bold font-sans text-center'>Candiat</h2>
+        <div className='flex justify-evenly '>
+   <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
+   
+  
+   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+       </div>
+       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+      
+       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+     
+       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans' >0</h3>
+       <br />
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+
+
+
+  </div>    
+  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+          <h2 className='font-bold font-sans text-center'>Candiat</h2>
+        <div className='flex justify-evenly '>
+   <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
+   
+  
+   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+       </div>
+       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+      
+       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+     
+       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans' >0</h3>
+       <br />
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+
+
+
+  </div>    
+  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+          <h2 className='font-bold font-sans text-center'>Candiat</h2>
+        <div className='flex justify-evenly '>
+   <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
+   
+  
+   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+       </div>
+       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+      
+       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+     
+       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans' >0</h3>
+       <br />
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+
+
+
+  </div>    
+  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+          <h2 className='font-bold font-sans text-center'>Candiat</h2>
+        <div className='flex justify-evenly '>
+   <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
+   
+  
+   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+       </div>
+       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+      
+       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+     
+       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans' >0</h3>
+       <br />
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+
+
+
+  </div>    
+  
+  
+  
+  
+    </div>
+    
+    </div>
+    
+  )
+}
+
+export default Candidature
