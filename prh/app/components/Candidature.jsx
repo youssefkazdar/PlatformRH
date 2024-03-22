ecb2e6a08cd8932b187bfc805354b3565a8bf5e9
youssefkazdar <youssefkazdar51@gmail.com>
2024-03-22 22:24:00 +0100

    r

--- prh/app/components/Candidature.jsx
+++ prh/app/components/Candidature.jsx
@@ -25,123 +25,125 @@ const Candidature = () => {
 
 
 
-    <div className='grid lg:grid-cols-3 gap-2'>
-        <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+    <div className='grid lg:grid-cols-3 gap-4'>
+        <div className='ml-4 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
           <h2 className='font-bold font-sans text-center'>Candiat</h2>
         <div className='flex justify-evenly '>
    <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
    
   
-   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+  
+   <h3 className='font-medium font-sans mt-6 ml-3' >Name familyName</h3>
        </div>
-       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+       <h3 className='font-medium font-sans ml-3' >Pole Choisi :</h3>
       
-       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+       <h3 className='font-thin font-sans ml-3' >Pole Projet et Prosection :</h3>
      
-       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
-       <h3 className='font-thin font-sans' >0</h3>
+       <h3 className='font-medium font-sans ml-3' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans ml-4' >0</h3>
        <br />
-       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20  mb-4 mr-4">Ouvrir fichier</button>
 
 
 
   </div>    
-  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+  <div className='ml-4 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
           <h2 className='font-bold font-sans text-center'>Candiat</h2>
         <div className='flex justify-evenly '>
    <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
    
   
-   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+   
+   <h3 className='font-medium font-sans mt-6 ml-3' >Name familyName</h3>
        </div>
-       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+       <h3 className='font-medium font-sans ml-3' >Pole Choisi :</h3>
       
-       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+       <h3 className='font-thin font-sans ml-3' >Pole Projet et Prosection :</h3>
      
-       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
-       <h3 className='font-thin font-sans' >0</h3>
+       <h3 className='font-medium font-sans ml-3' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans ml-4' >0</h3>
        <br />
-       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20  mb-4 mr-4">Ouvrir fichier</button>
+
+
+  </div>    
+  <div className='ml-4 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+          <h2 className='font-bold font-sans text-center'>Candiat</h2>
+        <div className='flex justify-evenly '>
+   <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
+   
+  
+  
+   <h3 className='font-medium font-sans mt-6 ml-3' >Name familyName</h3>
+       </div>
+       <h3 className='font-medium font-sans ml-3' >Pole Choisi :</h3>
+      
+       <h3 className='font-thin font-sans ml-3' >Pole Projet et Prosection :</h3>
+     
+       <h3 className='font-medium font-sans ml-3' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans ml-4' >0</h3>
+       <br />
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20  mb-4 mr-4">Ouvrir fichier</button>
 
 
 
   </div>    
-  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+  <div className='ml-4 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
           <h2 className='font-bold font-sans text-center'>Candiat</h2>
         <div className='flex justify-evenly '>
    <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
    
   
-   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+   
+   <h3 className='font-medium font-sans mt-6 ml-3' >Name familyName</h3>
        </div>
-       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+       <h3 className='font-medium font-sans ml-3' >Pole Choisi :</h3>
       
-       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+       <h3 className='font-thin font-sans ml-3' >Pole Projet et Prosection :</h3>
      
-       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
-       <h3 className='font-thin font-sans' >0</h3>
+       <h3 className='font-medium font-sans ml-3' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans ml-4' >0</h3>
        <br />
-       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20  mb-4 mr-4">Ouvrir fichier</button>
 
 
 
   </div>    
-  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+  <div className='ml-4 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
           <h2 className='font-bold font-sans text-center'>Candiat</h2>
         <div className='flex justify-evenly '>
    <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
    
   
-   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+   <h3 className='font-medium font-sans mt-6 ml-3' >Name familyName</h3>
        </div>
-       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+       <h3 className='font-medium font-sans ml-3' >Pole Choisi :</h3>
       
-       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+       <h3 className='font-thin font-sans ml-3' >Pole Projet et Prosection :</h3>
      
-       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
-       <h3 className='font-thin font-sans' >0</h3>
+       <h3 className='font-medium font-sans ml-3' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans ml-4' >0</h3>
        <br />
-       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
-
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20  mb-4 mr-4">Ouvrir fichier</button>
 
 
   </div>    
-  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
-          <h2 className='font-bold font-sans text-center'>Candiat</h2>
+  <div className='ml-4 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
+          <h2 className='font-bold font-sans text-center mt-2'>Candiat</h2>
         <div className='flex justify-evenly '>
    <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
    
   
-   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
+   <h3 className='font-medium font-sans mt-6 ml-3' >Name familyName</h3>
        </div>
-       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
+       <h3 className='font-medium font-sans ml-3' >Pole Choisi :</h3>
       
-       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
+       <h3 className='font-thin font-sans ml-3' >Pole Projet et Prosection :</h3>
      
-       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
-       <h3 className='font-thin font-sans' >0</h3>
+       <h3 className='font-medium font-sans ml-3' >Note d'evaluation :</h3>
+       <h3 className='font-thin font-sans ml-4' >0</h3>
        <br />
-       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
-
-
-
-  </div>    
-  <div className='ml-2 mb-2 border rounded-2xl   w-full max-w-64 shadow-lg bg-[#97aecd] text-black  '>
-          <h2 className='font-bold font-sans text-center'>Candiat</h2>
-        <div className='flex justify-evenly '>
-   <img src="https://via.placeholder.com/150" alt="" className="w-20 h-20 rounded-full object-cover" />
-   
-  
-   <h3 className='font-medium font-sans mt-6' >Name familyName</h3>
-       </div>
-       <h3 className='font-medium font-sans' >Pole Choisi :</h3>
-      
-       <h3 className='font-thin font-sans' >Pole Projet et Prosection :</h3>
-     
-       <h3 className='font-medium font-sans' >Note d'evaluation :</h3>
-       <h3 className='font-thin font-sans' >0</h3>
-       <br />
-       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20 ">Ouvrir fichier</button>
+       <button className=" self-center rounded-2xl py-1 px-6 bg-[#2F5B96] text-white ml-20  mb-4 mr-4">Ouvrir fichier</button>
 
 
 
